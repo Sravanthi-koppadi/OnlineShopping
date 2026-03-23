0cdd43196bb811f2b05efe10489b5eec4f5d7a03
Sravanthi Koppadi <85244113+Sravanthi-koppadi@users.noreply.github.com>
2026-03-23 14:15:05 +0530

    Add server route for product details
    Add server route for product details with server rendering.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,11 @@
+// app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'product/:id',
+    renderMode: RenderMode.Server // This tells Angular to build it on the fly, not at build-time
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
